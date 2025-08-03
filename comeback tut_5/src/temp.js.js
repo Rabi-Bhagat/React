adf599ebd9f9a0fa276215fdf11e3133d5240830
Rabi Bhagat <rabibhagat789@gmail.com>
2025-08-04 00:33:28 +0530

    preventdefault

--- comeback tut_5/src/temp.js.js
+++ comeback tut_5/src/temp.js.js
@@ -3,30 +3,63 @@ import React, { useState } from 'react'
 
 const Form = () => {
 
-  const [name,setname] = useState(" ")
+  const [name,setname] = useState(" ");
+  const [email,setemail] = useState(" ");
+  const [Password,setPassword] = useState(" ");
 
   const handlename = (e) =>{
-
-
     console.log(e.target.value);
     setname(e.target.value)
     
   }
 
+
+    const handleemail = (e) =>{
+    console.log(e.target.value);
+    setemail(e.target.value) 
+  }
+
+
+      const handlePassword = (e) =>{
+    console.log(e.target.value);
+    setPassword(e.target.value) 
+  }
+
+  const handlesubmit = (e) =>{
+    e.preventDefault();
+  }
   return (
-    <div>
+
+    <Form>
+
    
     <label>
-
     Name : 
-
-    <input type='text' onChange={handlename}>
+    <input type='text' value={name} onChange={handlename}>
     </input>
-
     </label>
 
+
+    <label>
+    Email : 
+    <input type='text' value={email} onChange={handleemail}>
+    </input>
+    </label>
+
+    <label>
+    Password: 
+    <input type='text' value={Password} onChange={handlePassword}>
+    </input>
+    </label>
+
+    <button></button>
+
     {name}
-    </div>
+    {email}
+    {Password}
+ 
+
+</Form>
   )
 }
 
